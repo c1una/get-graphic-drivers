b37b4b659883f655c3762d993c7220a55c4894d0
cluna <cluna@ccny.cuny.edu>
2022-02-08 00:42:49 -0500

    Nivida fetch driver function is working

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import wmi
+from os import system
 from time import sleep
 from urllib.request import urlopen
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-
 
 """ Example:
 # Nvidia url driver
@@ -18,6 +19,7 @@ https://drivers.amd.com/drivers/non-whql-radeon-software-adrenalin-2020-22.2.1-w
 
 NVIDIA_URL = "https://www.nvidia.com/Download/Find.aspx"
 AMD_URL = "https://www.amd.com/en/support"
+DOWNLOADS_PATH = str(Path.home() / "Downloads")
 
 
 def main():
@@ -39,23 +41,23 @@ def main():
         "product": "%s %s %s %s" % (gpu_info[1], gpu_info[2], gpu_info[3], gpu_info[4]),
     }
     # gpu_info : ['NVIDIA', 'GeForce', 'RTX', '2070', 'SUPER']
-
-    print(gpu["product_series"])
+    print("'{}' detected".format(gpu["product"]))
 
     def vendorUrl(vendor):
         match vendor:
             case "NVIDIA":
+                print("getting driver from {}".format(vendor))
                 return fetchNvidiaDriver(NVIDIA_URL)
             case "AMD":
+                print("getting driver from {}".format(vendor))
                 return fetchAMDDriver(AMD_URL)
-
-    # def fetchDriverUrl(url):
 
     def fetchNvidiaDriver(url):
         driver = webdriver.Chrome()
         driver.get(url)
         if not "Official" and not "AMD" in driver.title:
             raise Exception("could not load page")
+        print("fetching link...")
         # TODO: find a better way to do this. Breaking the DRY rule here : select1, select2
         select1 = Select(driver.find_element(By.ID, "selProductSeriesType"))
         option1 = None
@@ -85,14 +87,46 @@ def main():
             By.XPATH, "//*[@id='Table1']/tbody/tr/td/table[2]/tbody/tr/td/a"
         )
 
-        # TODO: figure out how to programmaticly select the first link from the table. Theres prob a method to get an array of link
         searchButton.click()
-        sleep(2)
+        driver.implicitly_wait(1)
+        driverLink = driver.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div[2]/div[3]/form/table[2]/tbody/tr/td/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/b/a",
+        )
+
+        driverLink.click()
+        driver.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div[2]/div[3]/table/tbody/tr/td/div/table[1]/tbody/tr[8]/td[1]/a",
+        ).click()
+        driver.implicitly_wait(1)
+        driverURL = driver.find_element(
+            By.XPATH,
+            "/html/body/div[2]/div/table/tbody/tr/td/div[3]/div[2]/table/tbody/tr/td/a",
+        ).get_attribute("href")
+
+        # print(driverURL)
         driver.quit()
+        downloadDriver(driverURL)
 
     def fetchAMDDriver():
+        # TODO:
         print()
-        # Instantiate headless driver
+
+    def downloadDriver(url):
+        clear = lambda: system("cls")
+        clear()
+        print(f"got the driver\n: ", url)
+        with urlopen(url) as file:
+            print("downloading to downloads folder...")
+            content = file.read()
+
+        with open("{}\\output2.exe".format(DOWNLOADS_PATH), "wb") as download:
+            download.write(content)
+
+        print("done!")
+        sleep(2)
+        return
 
     # # Windows path
     # ser = Service("./chromedriver_win32/chromedriver.exe")
@@ -101,55 +135,8 @@ def main():
     # # chrome_options.add_argument("--disable-dev-shm-usage")
     # chrome_options.add_argument("--disable-gpu")
 
-    # # chrome_prefs = {"download.default_directory": r"C:/Users/Chris/Downloads"}  # (windows)
-    # chrome_options.add_experimental_option(
-    #     "prefs",
-    #     {
-    #         "download.default_directory": "C:/Users/Chris/Downloads/new2",
-    #         "download.prompt_for_download": False,
-    #     },
-    # )
-
-    # chrome_options.experimental_options["prefs"] = chrome_prefs
-    # driver = webdriver.Chrome(service=ser, options=chrome_options)
-    # Download your file
-    # driver.implicitly_wait(4)
-    # driver.get("http://www.mockaroo.com/")
-    # driver.find_element(
-    #     By.XPATH, "//*[@id='__next']/main/div[2]/form/div[3]/div[1]/button"
-    # ).click()
-
-    # print(gpu["vendor"])
     url = vendorUrl(gpu["vendor"])
-    # url = vendorUrl("AMD")
-    # fetchDriverUrl(url)
 
 
 if __name__ == "__main__":
     main()
-
-# with urlopen() as file:
-#     content = file.read()
-
-# with open("C:\\Users\\Chris\\Downloads\\output.exe", "wb") as download:
-#     download.write(content)
-
-
-# time.sleep(10)
-# driver.close()
-# driver.find_element(By.CLASS_NAME, "MuiButtonGroup-root")
-# driver.find_element(By.CSS_SELECTOR, "button").click()
-
-# # computer_info = computer.Win32_ComputerSystem()[0]
-# # os_info = computer.Win32_OperatingSystem()[0]
-# # proc_info = computer.Win32_Processor()[0]
-
-# # os_name = os_info.Name.encode('utf-8').split(b'|')[0]
-# # os_version = ' '.join([os_info.Version, os_info.BuildNumber])
-# # system_ram = float(os_info.TotalVisibleMemorySize) / 1048576  # KB to GB
-
-# # print('OS Name: {0}'.format(os_name))
-# # print('OS Version: {0}'.format(os_version))
-# # print('CPU: {0}'.format(proc_info.Name))
-# # print('RAM: {0} GB'.format(system_ram))
-# # print('Graphics Card: {0}'.format(gpu_info.Name))
